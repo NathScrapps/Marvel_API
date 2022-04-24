1f3a81f37238eba87733a4f117147623b78022de
Natanael Corona <87894796+NathScrapps@users.noreply.github.com>
2022-04-24 00:54:00 -0500

    Update comic_price_API.php

--- assets/complements/comic_price_API.php
+++ assets/complements/comic_price_API.php
@@ -1,5 +1,5 @@
 <?php
-$url_price = "https://gateway.marvel.com:443/v1/public/comics/$comic_id?ts=$YOUR_TS_VALUE&apikey=$YOUR_HASH_MD5_VALUE";
+$url_price = "https://gateway.marvel.com:443/v1/public/comics/$comic_id?ts=$YOUR_TS_VALUE&apikey=$YOUR_PUBLIC_KEY&hash=$YOUR_HASH_MD5_VALUE";
 $options_url_connection=array(
     "ssl"=>array(
         "verify_peer"=>false,"verify_peer_name"=>false
