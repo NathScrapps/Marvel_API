d65fc04ae8387d85609a6c1b66611bf4a35ea015
Natanael Corona <87894796+NathScrapps@users.noreply.github.com>
2022-04-24 00:53:35 -0500

    Update character_comicAPI.php

--- assets/complements/character_comicAPI.php
+++ assets/complements/character_comicAPI.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://gateway.marvel.com:443/v1/public/comics/$comic_id/characters?orderBy=name&limit=52&ts=$YOUR_TS_VALUE&apikey=$YOUR_HASH_MD5_VALUE";
+$url = "https://gateway.marvel.com:443/v1/public/comics/$comic_id/characters?orderBy=name&limit=52&ts=$YOUR_TS_VALUE&apikey=$YOUR_PUBLIC_KEY&hash=$YOUR_HASH_MD5_VALUE";
 $opciones=array(
     "ssl"=>array(
         "verify_peer"=>false,"verify_peer_name"=>false
